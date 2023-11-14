e173ba3be2a7582d653df3b478853ac1aa8c16e0
hello-jesus <jesusrdz@hello-robot.com>
2023-11-14 15:02:49 -0800

    wip

--- RE2V0/stretch_description/generate_urdfs.py
+++ RE2V0/stretch_description/generate_urdfs.py
@@ -14,13 +14,10 @@ def run_cmd(cmdstr):
     return process
 
 def main():
-    print("---")
-    print("Convert the RE2 xacro files to uncalibrated URDF files.")
-    print("---")
 
-    #print("ros2 run xacro xacro `ros2 pkg prefix stretch_description`/urdf/stretch_description.xacro > `ros2 pkg prefix stretch_description`/urdf/stretch_uncalibrated.urdf")
-
-    descriptions=['./urdf/stretch_description_RE2V0_tool_none.xacro']
+    descriptions=['./urdf/stretch_description_RE2V0_tool_none.xacro',
+                  './urdf/stretch_description_RE2V0_tool_stretch_gripper.xacro',
+                  './urdf/stretch_description_RE2V0_tool_dex_wrist.xacro']
     urdfs=[]
     for d in descriptions:
         bashCommand = "ros2 run xacro xacro {}".format(d)
@@ -29,43 +26,15 @@ def main():
         urdf = process.stdout
         urdf_filepath = d[:-6]+'.urdf'
         urdfs.append(urdf_filepath)
+        urdf_lines=urdf.split('\n')
         with open(urdf_filepath, "w") as open_file:
-            print(urdf, file=open_file)
+            for u in urdf_lines:
+                if u.find('d435.dae')>-1: #Replace path for realsense
+                    ss=u.find('file://')
+                    u = u[:ss] + './meshes/d435.dae"/>'
+                print(u, file=open_file)
+
             open_file.close()
-
-    old_name = "package://stretch_description/"
-    new_name = "./"
-    for u in urdfs:
-        bashCommand='rpl -q --encoding UTF-8 %s %s %s'%(old_name,new_name,u)
-        print(bashCommand)
-        process = run_cmd(bashCommand)
-
-    #Now change path names
-
-    # bashCommand = "ros2 pkg prefix stretch_description"
-    # process = run_cmd(bashCommand)
-    # installpath = process.stdout
-    #
-    # # Following path manipulation is necessary because 'ros2 pkg prefix' command returns the /install path of the package whereas we want to make the changes in the /src path
-    # # For lack of a better comamnd in ROS 2 to directly fetch the /src path, we subtract the minuspath and add the newpath to the returned filepath
-    # addpath = "/src/stretch_ros2/stretch_description"
-    # minuspath = "/install/stretch_description"
-    # installpath = installpath[0 : len(installpath) - len(minuspath) - 1]
-    # srcpath = "{0}{1}".format(installpath, addpath)
-    #
-    # bashCommand = "ros2 run xacro xacro {}/urdf/stretch_description.xacro".format(srcpath)
-    # process = run_cmd(bashCommand)
-    # urdf = process.stdout
-
-    # test_filepath = "{}/urdf/stretch_uncalibrated.urdf".format(srcpath)
-    # with open(test_filepath, "w") as open_file:
-    #     print(urdf, file=open_file)
-    #     open_file.close()
-    #
-    # # Copy the stretch_uncalibrated.urdf in share directory as well
-    # uncalibrated_urdf_path = os.path.join(get_package_share_directory('stretch_description'), 'urdf', 'stretch_uncalibrated.urdf')
-    # bashCommand = "cp {0} {1}".format(test_filepath, uncalibrated_urdf_path)
-    # process = run_cmd(bashCommand)
 
 
 if __name__ == '__main__':
