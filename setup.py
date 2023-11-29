2b5aeef176cee213966449ef74aa38d4a3cd6f1e
aedsinger <aedsinger@hello-robot.com>
2023-11-29 11:04:42 -0800

    wip

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hello_robot_stretch_urdf",
-    version="0.0.9",
+    version="0.0.11",
     author="Hello Robot Inc.",
     author_email="support@hello-robot.com",
     description="Stretch URDF",
