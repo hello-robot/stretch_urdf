b85a45b44506c0e0209f9cdce5031316c43cd61c
Mohamed Fazil <mfazil@hello-robot.com>
2023-12-13 14:00:50 -0800

    Fix missing RE1,SE3 files on install

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hello_robot_stretch_urdf",
-    version="0.0.16",
+    version="0.0.17",
     author="Hello Robot Inc.",
     author_email="support@hello-robot.com",
     description="Stretch URDF",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hello-robot/stretch_urdf",
-    package_data={"stretch_urdf": ["RE2V0/meshes/*.STL","RE2V0/meshes/*.dae","RE2V0/*.urdf"]},
+    package_data={"stretch_urdf": ["RE2V0/meshes/*.STL","RE2V0/meshes/*.dae","RE2V0/*.urdf",
+                                   "RE1V0/meshes/*.STL","RE1V0/meshes/*.dae","RE1V0/*.urdf",
+                                   "SE3/meshes/*.STL","SE3/meshes/*.dae","SE3/*.urdf"]},
     packages=['stretch_urdf'],
     classifiers=[
         "Programming Language :: Python :: 3",
