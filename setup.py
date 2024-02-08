f078038e8806e4adbdba07a389ab7790bd743673
Mohamed Fazil <mfazil@hello-robot.com>
2024-02-07 22:36:35 -0800

    Update package data and bump version

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ stretch_scripts=[f for f in ex_scripts if isfile(f)]
 
 setuptools.setup(
     name="hello_robot_stretch_urdf",
-    version="0.0.19",
+    version="0.0.20",
     author="Hello Robot Inc.",
     author_email="support@hello-robot.com",
     description="Stretch URDF",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hello-robot/stretch_urdf",
-    package_data={"stretch_urdf": ["RE2V0/meshes/*.STL","RE2V0/meshes/*.dae","RE2V0/*.urdf",
-                                   "RE1V0/meshes/*.STL","RE1V0/meshes/*.dae","RE1V0/*.urdf",
-                                   "SE3/meshes/*.STL","SE3/meshes/*.dae","SE3/*.urdf"]},
+    package_data={"stretch_urdf": ["RE2V0/meshes/*.STL","RE2V0/meshes/*.dae","RE2V0/xacro/*.xacro","RE2V0/xacro/d435i/*.xacro","RE2V0/*.urdf",
+                                   "RE1V0/meshes/*.STL","RE1V0/meshes/*.dae","RE1V0/xacro/*.xacro","RE1V0/xacro/d435i/*.xacro","RE1V0/*.urdf",
+                                   "SE3/meshes/*.STL","SE3/meshes/*.dae","SE3/xacro/*.xacro","SE3/xacro/d435i/*.xacro","SE3/*.urdf"]},
     scripts=stretch_scripts,
     packages=['stretch_urdf'],
     classifiers=[
