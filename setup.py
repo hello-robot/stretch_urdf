78f8291d203d8d74115982498ab7faacdef0d37f
Mohamed Fazil <mfazil@hello-robot.com>
2024-06-21 15:29:01 -0700

    Add d405 assests to the package files

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ stretch_scripts=[f for f in ex_scripts if isfile(f)]
 
 setuptools.setup(
     name="hello-robot-stretch-urdf",
-    version="0.0.26",
+    version="0.0.27",
     author="Hello Robot Inc.",
     author_email="support@hello-robot.com",
     description="Stretch URDF",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hello-robot/stretch_urdf",
-    package_data={"stretch_urdf": ["RE2V0/meshes/*.STL","RE2V0/meshes/*.dae","RE2V0/xacro/*.xacro","RE2V0/xacro/d435i/*.xacro","RE2V0/*.urdf",
+    package_data={"stretch_urdf": ["RE2V0/meshes/*.STL","RE2V0/meshes/*.dae","RE2V0/xacro/*.xacro","RE2V0/xacro/d435i/*.xacro","RE2V0/xacro/d405/*.xacro","RE2V0/*.urdf",
                                    "RE1V0/meshes/*.STL","RE1V0/meshes/*.dae","RE1V0/xacro/*.xacro","RE1V0/xacro/d435i/*.xacro","RE1V0/*.urdf",
-                                   "SE3/meshes/*.STL","SE3/meshes/*.dae","SE3/xacro/*.xacro","SE3/xacro/d435i/*.xacro","SE3/*.urdf"]},
+                                   "SE3/meshes/*.STL","SE3/meshes/*.dae","SE3/xacro/*.xacro","SE3/xacro/d435i/*.xacro","SE3/xacro/d405/*.xacro","SE3/*.urdf"]},
     scripts=stretch_scripts,
     packages=['stretch_urdf'],
     classifiers=[
